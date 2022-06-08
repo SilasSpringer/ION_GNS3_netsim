f0544a9d714367e5b0d932691ba6c34c8759e163
SilasSpringer <ss132517@ohio.edu>
2022-06-08 16:09:52 -0400

    added basic JSON config file for python script file. also updated script with comments, added ipn number annotation to node startup.

--- PYSERVER/START_PY_SERVER_1.py
+++ PYSERVER/START_PY_SERVER_1.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import gns3fy
 
 from tabulate import tabulate
@@ -6,23 +7,66 @@ import subprocess
 
 # host and project name need to be specified by user/script.
 
-subnet="192.168.0."
 
+# BEGIN CONFIG FILE
+
+# TODO: add security checks to make sure illegal configuration options are 
+# spit out as warnings to commandline, and default values are used instead of garbage.
+
+with open("pyscript.conf", "r") as conffile:
+	conf = json.load(conffile)
+
+try:
+	debug = conf['debug']
+	if debug == "True" or debug == "true":
+		debug = True
+		print("debug enabled")
+	else:
+		debug = False
+except:
+	debug = False
+	
+try:
+	netaddr = str(conf['netaddr'] + ".")
+except:
+	netaddr = "192.168.0."
+
+try:
+	serverptcl = conf['gns3serverptcl']
+except:
+	serverptcl = "http"
+
+try:
+	serverport = conf['gns3serverport']
+except:
+	serverport = "3080"
+
+try:
+	linkprotocol = conf['linkprotocol']
+except:
+	linkprotocol = "tcp"
+# END CONFIG FILE
+
+# BEGIN COMMANDLINE INPUT
 numargs = len(sys.argv)
 
 if numargs > 3 or numargs < 2:
 	sys.exit(str("usage: python3 START_PY_SERVER_1.py <gns3serverhost> <projectname (optional)>\nnum args was " + str(numargs)))
 
 serverhost = str(sys.argv[1])
+
 if numargs == 3:
 	projectname = str(sys.argv[2])
 else:
 	projectname = None
+# END COMMANDLINE INPUT
 
-print("server is: " + "http://" + serverhost + ":3080")
+if debug:
+	print("server is: " + serverptcl + "://" + serverhost + ":" + serverport)
 
-server	= gns3fy.Gns3Connector("http://" + serverhost + ":3080")
+server	= gns3fy.Gns3Connector(serverptcl + "://" + serverhost + ":" + serverport)
 
+# get the active project name if no project name was supplied.
 if projectname is None:
 	projects = server.projects_summary(is_print=False)
 	for proj in projects:
@@ -30,41 +74,36 @@ if projectname is None:
 			projectname = proj[0]
 			project_id = proj[1]
 			break
-else:
+# if the project name was supplied, find it in the list of projects on the GNS3 server and record its project ID
+else: 
 	projects = server.projects_summary(is_print=False)
 	for proj in projects:
 		if proj[0] == projectname:
 			project_id = proj[1]
 			break
-
+# open project and fetch details
 project = gns3fy.Project(name=projectname, connector=server)
 project.get()
-nodes 	= project.nodes_summary(is_print=False)#project.nodes
-links	= project.links_summary(is_print=False)#project.links
 
-'''
-now iterate through each node and link, and make an ip for each node and interface
-proferably base the ips off the interface number and node number in some way to be able to engineer them inside the containers too to configure ION. if not, will directly conf ion using docker exec in the .sh script.
-'''
-# limited at 24 nodes, 10 adapters
-# nodes go from 1 to 25, adapters from 0 to 9.
-'''ina = [] # ina - ip, nodename, adapter - contains a list of all nodes, where each node is represented by a pair that has the node name, and an array containing pairs matching each interface to an ip address.
+# get a node and link summary
+nodes 	= project.nodes_summary(is_print=False)
+links	= project.links_summary(is_print=False)
+
+# now iterate through each node and link, and make an ip for each node and interface
+# limited at 24 nodes, 10 adapters using current method.
+# nodes go from 1 to 24, adapters from 0 to 9. (with a final address range of <netaddr>.1 to <netaddr>.249 )
+
+# ina - ip, nodename, adapter - contains a list of all nodes, 
+# where each node is represented by a pair that has the node name, 
+# and an array containing pairs matching each interface to an ip address.
+ina = [] 
 for i, node in enumerate(project.nodes):
 	ips = [] # temporary storage array for the ip-adapter pairs 
 	for index, adapter in enumerate(node.ports): # for every adapter in the ports section,
 		if int( str(i+1) + str(index) ) > 255:
 			sys.exit("ERROR: cannot have more than 24 nodes or 10 adapters, as the ip generation method is limited in possibilities. Please reduce your network to meet requirements.")
-		ips.append((adapter['name'], str(subnet + str(i+1) + str(index) + "/24")))
-		# calculate a unique IP address for that adapter.	
-	ina.append((node.name, i+1, ips, [(i+1, 127.0.0.1)]))
-'''
-ina = [] # ina - ip, nodename, adapter - contains a list of all nodes, where each node is represented by a pair that has the node name, and an array containing pairs matching each interface to an ip address.
-for i, node in enumerate(project.nodes):
-	ips = [] # temporary storage array for the ip-adapter pairs 
-	for index, adapter in enumerate(node.ports): # for every adapter in the ports section,
-		if int( str(i+1) + str(index) ) > 255:
-			sys.exit("ERROR: cannot have more than 24 nodes or 10 adapters, as the ip generation method is limited in possibilities. Please reduce your network to meet requirements.")
-		ips.append((adapter['name'], str(subnet + str(i+1) + str(index))))
+		# TODO: use a better method or at least make the mehtod configurable for the way ips are generated to allow for larger networks.
+		ips.append((adapter['name'], str(netaddr + str(i+1) + str(index))))
 		# calculate a unique IP address for that adapter.	
 	ina.append({
 		'name' : node.name, 
@@ -72,12 +111,15 @@ for i, node in enumerate(project.nodes):
 		'interfaces' : ips, 
 		'neighbors' : [] 
 		})
+	
 
 
 # TODO: clean this up to be more efficient, maybe use a hashmap to store the modes for faster lookup?
 for link in links:
 	connected=[0,0] # store node numbers of connected nodes in this link
-	conn_ips=["127.0.0.1", "127.0.0.1"] # store ip addresses at each end of this link
+	conn_ips=["127.0.0.1", "127.0.0.1"] # store ip addresses at each end of this link. 
+	#TODO add security checks to make sure that all nodes have correct ips, rather than 
+	# hoping it all works as intended. currently could recieve a loopback ip (127.0.0.1) if the correct ip isnt found.
 	
 	# link is name-iface-name-iface
 	for ix, node in enumerate(ina):
@@ -97,27 +139,33 @@ for link in links:
 	ina[connected[0]-1]['neighbors'].append((connected[1], conn_ips[1]))
 	ina[connected[1]-1]['neighbors'].append((connected[0], conn_ips[0]))
 			
-# start nodes ( need to remove entrypoint configurator )
-# also runs commands to assign IP addresses on each node for each interface.
+
+# starts nodes and also runs commands to assign IP addresses on each node for each interface.
+# TODO: potentially shift away from using docker exec to run commands on the nodes, or at least 
+# shift to opening a pseudoterminal in the node and submitting commands sequentially, 
+# rather than submitting each command separately through its own exec command subprocess
 node_accessors = []
 for j, node in enumerate(ina):
 	node_accessors.append(gns3fy.Node(project_id=project.project_id, name=node['name'], connector=server))
+	# temporary solution for adding the IPN numbers to the names of each node as they start. 
+	# TODO: update this to read the ipn number from the title if it exists, so the user can specify ipn numbers.
+	if node['name'].find('ipn:') == -1:
+		node_accessors[j].update(name=str(node['name'] + "-ipn:" + str(node['number'])))
 	node_accessors[j].start() # start node
 	# assign ips to each interface on node.
 	for iface in node['interfaces']:
 		command=["docker", "exec", "-t", str(node_accessors[j].properties['container_id'])]
 		command.extend(['ip', 'addr', 'add', str(iface[1]+"/24"), 'dev', iface[0]])
-		subprocess.run(command)
+		subprocess.run(command, stdout=subprocess.DEVNULL)
 	
 	#./conf_script.sh <nodenum> <numneighbors> <neighbors> nx.ionrc nx.ionconfig nx.ltprc
 	# TODO: move config directory up a layer to allow access regardless of ion config version.
-	# TODO: remove debug print, or at least disable debug print in container conf_script.
 	command = ['docker', 'exec', '-t', '-w', '/ion-open-source-4.1.0/ion_config/', str(node_accessors[j].properties['container_id']),
 	'./conf_script.sh', str(node['number']), str(len(node['neighbors']))]
 	neighbors = map( lambda x: str(x[0]), node['neighbors'] )
 	command.extend(neighbors)
-	command.extend(['nx.ionrc','nx.ionconfig','nx.ltprc'])
-	subprocess.run(command)
+	command.extend(['nx.ionrc','nx.ipnrc','nx.bprc'])# currently unconfigurable files: 'nx.ionconfig' 'nx.ltprc'
+	subprocess.run(command, stdout=subprocess.DEVNULL)
 
 # use link information and the matrix of tuples made above to configure the .bprc and .ipnrc files on each node.
 
@@ -126,11 +174,15 @@ for j, node in enumerate(ina):
 # start all config files in appropriate admin service (bpadmin, ionadmin, etc.)
 
 
+if debug:
+	print("project name: ")
+	print(project.name)
+	print("project id: ")
+	print(project_id)
+	print("node summary:")
+	print(nodes)
+	print("link summary:")
+	print(links)
 
-print(project.name)
-print(project_id)
-print(nodes)
-print(links)
-
-print("\n\n")
-print(tabulate(ina))
+	print("\nina - node, adapter, and ip summary table:")
+	print(tabulate(ina))
