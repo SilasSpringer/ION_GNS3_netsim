42b6ca940e09a0f5b77f008de64c4bba1e24e60f
SilasSpringer <ss132517@ohio.edu>
2022-07-02 14:50:01 -0400

    consolidated both dockerfiles into one, and updated make_ion_image script to work correctly with the single dockerfile. also updated name of dockerfile to allow for swapping to  a system where one may pull an image instead ofbuilding it. currently not implemented.

--- docker_resources/make_ion_image.sh
+++ docker_resources/make_ion_image.sh
@@ -2,11 +2,9 @@
 
 # temporary solution. will add a top level makefile or script that packages all of this later along with installing docker and gns3 if not already installed
 
-docker build -t ion_base_image:0.1 - < DOCKERFILE_COMP
-
 cd dockercontext/
 
-docker build -t ion_2node:1.0 .
+docker build -t sspringe/ion_node_ubuntu_latest:1.1 .
 
 echo "2-node image built and tagged as ion_2node:1.0"
 echo "base ion image built and tagged as ion_base_image:0.1"
